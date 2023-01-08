4d8dac94f5e85171eb5968583384c1a58c72b47f
seth-gd <u443447760@byui.edu>
2023-01-07 20:25:37 -0600

    Finished Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Ask the user for input; define and parse variables
+        Console.Write("What is your grade? ");
+        string grade = Console.ReadLine();
+        int gradeNum = int.Parse(grade);
+
+        string gradeLetter;
+
+        // Determine the letter grade
+        if (gradeNum >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradeNum >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradeNum >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradeNum >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {gradeLetter}");
+
+        // Tell the user if the grade is a passing grade
+        if (gradeNum >= 70) {
+            Console.WriteLine("Congratulations! You passed the semester.");
+        }
+        else {
+            Console.WriteLine("You failed the semester. Try again next time.");
+        }
     }
 }
