1e026346d37d7dd6f1c5f00660c9e8eeae8635e3
seth-gd <u443447760@byui.edu>
2023-01-09 15:18:33 -0600

    Finished Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Get the magic number
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 101);
+
+        // Declare the variables used in the do while loop
+        string userInput;
+        int userNum;
+
+        // Run the game loop
+        do
+        {
+            // Ask the user to take a guess
+            Console.Write("What is your guess? ");
+            userInput = Console.ReadLine();
+            userNum = int.Parse(userInput);
+
+            // Determine if the user guessed right
+            if (userNum > magicNum) 
+            {
+                Console.WriteLine("lower");
+            }
+            else if (userNum < magicNum)
+            {
+                Console.WriteLine("higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (userNum != magicNum);
     }
 }
