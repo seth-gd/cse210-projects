e1ec5e3ee53870cc358304e5e8cd680de88f81ff
seth-gd <u443447760@byui.edu>
2023-01-18 17:34:47 -0600

    Finished Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "McDonalds";
+        job1._jobTitle = "Manager";
+        job1._startYear = 2020;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._company = "Waffle House";
+        job2._jobTitle = "Dish Washer";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume resume = new Resume();
+        resume._name = "Eduardo Pansa";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.DisplayResume();
     }
 }
