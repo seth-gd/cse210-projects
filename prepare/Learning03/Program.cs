64220defe43821d36557d0ad2e074b4507f4e66f
seth-gd <u443447760@byui.edu>
2023-02-01 16:57:06 -0600

    Finished Learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        fraction1.SetTop(5);
+        fraction1.SetBottom(10);
+        Console.WriteLine(fraction1.GetTop());
+        Console.WriteLine(fraction1.GetBottom());
+
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
     }
 }
